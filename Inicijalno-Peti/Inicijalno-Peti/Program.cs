2fd326344bf4d0ce3b65e1b725d448296849d96c
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-10-16 23:43:50 +0200

    Add files via upload

---
+++ Inicijalno-Peti/Inicijalno-Peti/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno_Peti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url;
+
+            Console.WriteLine("Upišite URL:");
+            url = Console.ReadLine();
+
+            if (url.StartsWith("www.") && url.EndsWith(".hr"))
+            {
+                Console.WriteLine("URL je pravilan");
+            }
+            else
+            {
+                Console.WriteLine("URL nije pravilan");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
